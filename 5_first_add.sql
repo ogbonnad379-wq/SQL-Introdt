2bfe228519c040d1fdd5996b07e5422947d587ca
David <ogbonnad379@gmail.com>
2025-09-16 16:38:57 +0100

    inserted new rows in the table

---
+++ 5_first_add.sql
@@ -0,0 +1,7 @@
+-- the SQL statement below inserts a new row in the table first_table (database simba_group) in your PostgreSQL Server.
+
+
+INSERT INTO first_table  (id, name)
+VALUES
+ (89, 'Best Company');
+ 
